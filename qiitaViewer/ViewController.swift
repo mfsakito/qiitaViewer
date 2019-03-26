f48f1c35132c30dc3d596fee1e16490ba537772b
古賀旺人 <akitokoga@akito-2.local>
2019-03-26 19:42:34 +0900

    Initial Commit

---
+++ qiitaViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiitaViewer
+//
+//  Created by 古賀旺人 on 2019/03/26.
+//  Copyright © 2019 古賀旺人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ qiitaViewerTests/qiitaViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  qiitaViewerTests.swift
+//  qiitaViewerTests
+//
+//  Created by 古賀旺人 on 2019/03/26.
+//  Copyright © 2019 古賀旺人. All rights reserved.
+//
+
+import XCTest
+@testable import qiitaViewer
+
+class qiitaViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
