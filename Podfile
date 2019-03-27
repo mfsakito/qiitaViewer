e7984fb96bd0fa6c6b23df94745efda6fc4d5975
akitokoga <mfsakito@gmail.com>
2019-03-27 22:29:00 +0900

    以下を参考に、新着記事のタイトルとIDが表示されるようにした
    https://qiita.com/shun6934/items/f4600c2a2e8b45d1219e#_reference-b442d2d1d8b743308ebd

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'qiitaViewer' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod "Alamofire"
+  pod "SwiftyJSON"
+  pod "RealmSwift"
+  # Pods for qiitaViewer
+
+  target 'qiitaViewerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'qiitaViewerUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- qiitaViewer/ViewController.swift
+++ qiitaViewer/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+var articles:[[String:String?]] = []
+
+func getArticles(){
+    let _ = Alamofire.request("https://qiita.com/api/v2/items").responseJSON{
+        response in
+        guard let object = response.result.value else {
+            return
+        }
+        
+        let json = JSON(object)
+        json.forEach {(_,json) in
+            let article:[String:String?] = [
+            "title":json["title"].string,
+            "url":json["url"].string,
+            "userId":json["user"]["id"].string
+            ]
+            self.articles.append(article)
+            
+        }
+    self.articleTableView.reloadData()
+    }
+    
+}
+
+
+    
+    @IBOutlet weak var articleTableView: UITableView!
+    
+    //TableViewに表示させるもの
+    func tableView(_ tableView:UITableView,numberOfRowsInSection section :Int) -> Int{
+        return articles.count
+    }
+    
+    //tableViewCellの内容
+    func tableView(_ tableView:UITableView,cellForRowAt indexPath:IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style:.subtitle,reuseIdentifier:"cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]!
+        cell.detailTextLabel?.text = article["userId"]!
+        return cell
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getArticles()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
+    
+    
 }
 
