82e16301a95926db0a6a24d3cc29b101642a1a4a
mfsakito <mfsakito@gmail.com>
2019-04-13 16:44:24 +0900

    apiからとってきたjsonをクラスで扱うように修正

---
+++ qiitaViewer/Article.swift
@@ -0,0 +1,26 @@
+//
+//  Article.swift
+//  qiitaViewer
+//
+//  Created by 古賀旺人 on 2019/04/13.
+//  Copyright © 2019 古賀旺人. All rights reserved.
+//
+
+import Foundation
+
+    class Article{
+        
+        var title:String!
+        var url:String!
+        var userId:String!
+        var profileImg:String!
+
+        init(title:String,url:String,userId:String,profileImg:String)
+        {
+        self.title = title
+        self.url = url
+        self.userId = userId
+        self.profileImg = profileImg
+        }
+
+    }
--- qiitaViewer/ViewController.swift
+++ qiitaViewer/ViewController.swift
@@ -18,8 +18,9 @@ import RealmSwift
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
-var articles:[[String:String?]] = []
-
+    var articleDataArray = [Article]()
+    var imageCache = NSCache<AnyObject, AnyObject>()
+    
 func getArticles(){
     let _ = Alamofire.request("https://qiita.com/api/v2/items").responseJSON{
         response in
@@ -29,14 +30,18 @@ func getArticles(){
         
         let json = JSON(object)
         json.forEach {(_,json) in
-            let article:[String:String?] = [
-            "title":json["title"].string,
-            "url":json["url"].string,
-            "userId":json["user"]["id"].string,
-            "profile_img":json["user"]["profile_image_url"].string
-            ]
-            self.articles.append(article)
-            print(self.articles)
+            let article = Article(title:String,url:String,userId:String,profileImg:String)
+            if let title = json["title"].string{
+                article.title = title}
+            if let url = json["url"].string {
+                article.url = url }
+            if let userId = json["user"]["id"].string {
+                article.userId = userId}
+            if let profileImg = json["user"]["profile_image_url"].string {
+                article.profileImg = profileImg}
+            
+            self.articleDataArray.append(article)
+            print(self.articleDataArray)
         }
     self.articleTableView.reloadData()
     }
@@ -46,8 +51,8 @@ func getArticles(){
     var selectUrl: String!
 //    cellがタップされたとき
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let newArticle = articles[indexPath.row]
-        selectUrl = newArticle["url"]!
+        let newArticle = articleDataArray[indexPath.row]
+        selectUrl = newArticle.url
         performSegue(withIdentifier: "toNewViewController", sender: nil)
     }
 //    画面遷移のとき
@@ -60,16 +65,16 @@ func getArticles(){
     
     //TableViewに表示させるもの
     func tableView(_ tableView:UITableView,numberOfRowsInSection section :Int) -> Int{
-        return articles.count
+        return articleDataArray.count
     }
     
     //tableViewCellの内容
     func tableView(_ tableView:UITableView,cellForRowAt indexPath:IndexPath) -> UITableViewCell{
         let cell = UITableViewCell(style:.subtitle,reuseIdentifier:"cell")
-        let article = articles[indexPath.row]
+        let article = articleDataArray[indexPath.row]
         cell.imageView?.image = UIImage(named: "logo.png")
-        cell.textLabel?.text = article["title"]!
-        cell.detailTextLabel?.text = article["userId"]!
+        cell.textLabel?.text = article.title
+        cell.detailTextLabel?.text = article.userId
 
 
 //ココから。サムネの初期値はいれられたので、非同期で画像をためていきたい。
@@ -97,21 +102,6 @@ func getArticles(){
     }
 
     
-//    model化。30行目付近のjsonの管理に関わる。
-//    別ファイルに分けたほうがいいかも。
-
-//    class Article:NSObject{
-//        var title:String,url:String,userId:String,profileImg:String
-//
-//        init(title:String,url:String,userId:String,profileImg:String)
-//        {
-//        self.title = title
-//        self.url = url
-//        self.userId = userId
-//        self.profileImg = profileImg
-//        }
-//
-//    }
     
 }
 
